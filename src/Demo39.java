c5900c691a0dd750fe877689594aee305364e9d1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-09-26 19:37:10 +0530

    Demo39.java

---
+++ src/Demo39.java
@@ -0,0 +1,9 @@
+public class Demo39 {
+    void main() throws Exception{
+        int x = 0;
+        while (x++ < 5) {
+            System.out.println("Dnnam athi");
+            Thread.sleep(5000);
+        }
+    }
+}
