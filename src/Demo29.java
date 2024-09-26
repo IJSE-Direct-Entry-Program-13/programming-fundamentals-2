0130620f6934de8653f2065e72be4a9cc5e46184
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-09-26 11:52:36 +0530

    Add new demos

--- src/Demo29.java
+++ src/Demo29.java
@@ -1,3 +1,19 @@
-void main(){
+import java.util.Scanner;
 
+void main(){
+    final String RED = "\u001b[31;1m";
+    final String RESET = "\u001b[0m";
+    var scanner = new Scanner(System.in);
+    boolean invalid = true;
+    String name = "";
+
+    while (invalid) {
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine().strip();
+
+        if (invalid = (name.isBlank())) System.out.println(STR."\{RED}Name can't be empty\{RESET}");
+        else if (invalid = (name.length() < 3)) System.out.println(STR."\{RED}Invalid name\{RESET}");
+    }
+
+    if (!invalid) System.out.println(STR."Hi \{name}!");
 }
