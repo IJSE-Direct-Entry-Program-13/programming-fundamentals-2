69f4eb23acc95dc9921f4942372a544a23e7b657
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-09-30 19:04:48 +0530

    Add demos

--- src/Demo47.java
+++ src/Demo47.java
@@ -17,22 +17,31 @@ void main() {
             message = "Default size (9x9) is selected";
             size = 9;
         } else {
-            if ((size - 9) % 2 == 0){
+            if ((size - 9) % 2 == 0) {
                 message = STR."Size (\{size}x\{size}) is selected";
-            }else{
+            } else {
                 size--;
-                message =STR."Size (\{size}x\{size}) is selected";
+                message = STR."Size (\{size}x\{size}) is selected";
             }
         }
     }
     System.out.println(STR."\{BLUE}\{message}\{RESET}");
 
-    for (int i = 0,x = 0, y = 0; i < (size+1) * size; i++, x++) {
-        System.out.print(STR."(\{x},\{y})");
-        if (x == size){
+    for (int i = 0, x = 0, y = 0; i < (size + 1) * size; i++, x++) {
+        if (x != size) {
+            if (y == x || y == 8 - x ||
+                    (x == 0 || x == (size - 1)) && (y == 2 || y == (size - 3)) ||
+                    (x == 2 || x == (size - 3)) && (y == 0 || y == (size - 1))) {
+                System.out.print("* ");
+            } else {
+                System.out.print(". ");
+            }
+        } else {
             System.out.println();
-           y++;
-           x = 0;
+        }
+        if (x == size) {
+            y++;
+            x = -1;
         }
     }
 }
